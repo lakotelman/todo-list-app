fce4bfd0b043b6462c1221a96549b7b60de2ca1c
lakotelman <la.n.kot@pm.me>
2022-07-20 06:09:21 -0800

    first commit

---
+++ localstorage.js
@@ -0,0 +1,67 @@
+let i = 0;
+let allEntries = [];
+
+
+let formEl = document.getElementById("toDoForm");
+let listEl = document.getElementById("lists");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let titleInput = document.getElementById("listTitle");
+  let itemsInput = document.getElementById("listItems");
+
+  if (titleInput.value !== "" && itemsInput.value !== "") {
+    let itemHTML = "";
+    for (let item of itemsInput.value.split("\n")) {
+      itemHTML += `
+        <li>${item}</li>
+      `;
+    }
+    let toDoHTML = `
+            <div class="card" id="card-${i}">
+                <div class="card-body">
+                    <h4 class="card-title">${titleInput.value}</h4>
+                    <ul>${itemHTML}</ul>
+                </div>
+                <div class= container>
+                <button id="toDoDone" class="btn btn-dark mb-3">All Done!</button>
+                </div>
+            </div>
+        `;
+    // listEl.innerHTML += toDoHTML;
+    allEntries.push(toDoHTML)
+    window.localStorage.setItem('storageKey', JSON.stringify(allEntries));
+
+
+    let itemEls = document.querySelectorAll(`#card-${i} li`);
+    for (let itemEl of itemEls) {
+      itemEl.addEventListener("click", (event) => {
+        if (itemEl.style.textDecoration != "line-through") {
+          itemEl.style.textDecoration = "line-through";
+        } else {
+          itemEl.style.textDecoration = "none";
+        }
+      });
+    }
+
+    titleInput.value = "";
+    itemsInput.value = "";
+    i++;
+  }
+});
+
+let clearButton = document.getElementById("clearButton");
+
+clearButton.addEventListener("click", (event) => {
+  console.log("Button clicked");
+  listEl.innerHTML = "";
+});
+
+let huh = JSON.parse(localStorage.getItem('storageKey'))
+for(let entry of JSON.parse(localStorage.getItem("storageKey"))){
+console.log(entry)
+formEl.innerHTML += entry
+}
+
+//How to remove individual listboxes
+//How to get the Local Storage to work whhhyy?
---
+++ main.js
@@ -0,0 +1,52 @@
+let i = 0;
+
+let formEl = document.getElementById("toDoForm");
+let listEl = document.getElementById("lists");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let titleInput = document.getElementById("listTitle");
+  let itemsInput = document.getElementById("listItems");
+
+  if (titleInput.value !== "" && itemsInput.value !== "") {
+    let itemHTML = "";
+    for (let item of itemsInput.value.split("\n")) {
+      itemHTML += `
+        <li>${item}</li>
+      `;
+    }
+    let toDoHTML = `
+            <div class="card" id="card-${i}">
+                <div class="card-body">
+                    <h4 class="card-title">${titleInput.value}</h4>
+                    <ul>${itemHTML}</ul>
+                </div>
+                <div class= container>
+                <button id="toDoDone" class="btn btn-dark mb-3">All Done!</button>
+                </div>
+            </div>
+        `;
+    listEl.innerHTML += toDoHTML;
+    let itemEls = document.querySelectorAll(`#card-${i} li`);
+    for (let itemEl of itemEls) {
+      itemEl.addEventListener("click", (event) => {
+        if (itemEl.style.textDecoration != "line-through") {
+          itemEl.style.textDecoration = "line-through";
+        }
+        else{ 
+          itemEl.style.textDecoration = "none"
+        }
+      });
+    }
+
+    titleInput.value = "";
+    itemsInput.value = "";
+    i++;
+  }
+});
+
+let clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', (event) => {
+    console.log('Button clicked')
+    listEl.innerHTML = ''
+})
